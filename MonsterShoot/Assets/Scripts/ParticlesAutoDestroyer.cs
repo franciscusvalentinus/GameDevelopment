5314d2212edc620e1120dda3c81f9f1f43ac9c33
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-02-14 10:54:38 +0700

    MonsterShoot

---
+++ MonsterShoot/Assets/Scripts/ParticlesAutoDestroyer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlesAutoDestroyer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private ParticleSystem ps;
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!ps.IsAlive())
+        {
+            Destroy(gameObject);
+        }
+    }
+}
