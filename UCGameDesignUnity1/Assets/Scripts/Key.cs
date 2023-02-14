58d82fd1065c3f2a51d90c89927edc9e385a245c
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-02-14 10:50:27 +0700

    UCGameDesignUnity1

---
+++ UCGameDesignUnity1/Assets/Scripts/Key.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    [SerializeField] GameObject shockwavePrefab;
+    [SerializeField] GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) {
+            gameManager.gameOver = true;
+            gameManager.GameOver();
+            Instantiate(shockwavePrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject, 0.1f);
+        }
+    }
+}
---
+++ UCGameDesignUnity1/Assets/Scripts/LockedOn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockedOn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject target;
+    Vector3 distance;
+    void Start()
+    {
+        distance = target.transform.position - this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = target.transform.position - distance;
+    }
+}
